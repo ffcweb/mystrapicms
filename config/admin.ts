bbf50d3da0e4e686177401f96117ef4ef33bd5d6
fangfang <fang@fang-mac-air.local>
2025-06-15 02:11:58 -0400

    Initial commit

--- config/admin.ts
+++ config/admin.ts
@@ -7,7 +7,7 @@ export default ({ env }) => ({
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT', 'anotherRandomLongString'),
     },
   },
   secrets: {
